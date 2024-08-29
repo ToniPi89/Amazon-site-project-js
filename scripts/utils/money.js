ee6b72a0a031bc65fa410a69a47591a2e190807a
ToniPi89 <tonipinjuh89@gmail.com>
2024-08-30 01:51:18 +0200

    save data and generate html for payment summary

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,5 +1,5 @@
 export function formatCurrency(priceCents) {
-  return (priceCents / 100).toFixed(2);
+  return (Math.round(priceCents) / 100).toFixed(2); // issue with rounding, i put Math.round to fix it
 }
 
 export default formatCurrency;
