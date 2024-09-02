2e56174600f4266c21d03fb51a1b1823437e3b7f
ToniPi89 <tonipinjuh89@gmail.com>
2024-09-02 15:43:39 +0200

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentsummary } from "./checkout/paymentSummary.js";
+import "../data/cart-oop.js";
 
 renderOrderSummary();
 renderPaymentsummary();
