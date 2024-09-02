6959156d1ac8a3467d196d1e59f10c353c2aef9d
ToniPi89 <tonipinjuh89@gmail.com>
2024-09-02 21:07:44 +0200

    Convert products into classes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -42,7 +42,7 @@ js-cart-item-container-${matchingProduct.id}">
       ${matchingProduct.name}
     </div>
     <div class="product-price">
-      $${formatCurrency(matchingProduct.priceCents)}
+      ${matchingProduct.getPrice()}
     </div>
     <div class="product-quantity
     js-product-quantity-${matchingProduct.id}">
@@ -52,9 +52,9 @@ js-cart-item-container-${matchingProduct.id}">
       <span class="update-quantity-link link-primary">
         Update
       </span>
-      <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${
+      <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${
         matchingProduct.id
-      }">
+      }" data-product-id="${matchingProduct.id}">
         Delete
       </span>
     </div>
