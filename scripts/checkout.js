b756655492dce33a1ea1a2cf670858c0f0dae1a3
ToniPi89 <tonipinjuh89@gmail.com>
2024-09-03 17:06:24 +0200

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,11 @@
+import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentsummary } from "./checkout/paymentSummary.js";
-//import "../data/cart-class.js";
-import "../data/backend-practice.js";
 
-renderOrderSummary();
-renderPaymentsummary();
+//import "../data/cart-class.js";
+//import "../data/backend-practice.js";
+
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentsummary();
+});
